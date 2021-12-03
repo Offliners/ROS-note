eb711114f1d49805b1c5cb2bfc015b8f17985bd5
Offliner <40673034h@ntnu.edu.tw>
2021-12-03 15:41:12 +0800

    Create talker.cpp

---
+++ ros_example_ws/src/ros1_talker_cpp/src/talker.cpp
@@ -0,0 +1,27 @@
+#include"ros/ros.h"
+#include"std_msgs/String.h"
+#include<sstream>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ros1_talker_cpp");
+    ros::NodeHandle n;
+    ros::Publisher pub = n.advertise<std_msgs::String>("/chatter", 1000);
+    ros::Rate loop_rate(2);
+
+    int count = 0;
+    while(ros::ok())
+    {
+        std_msgs::String msg;
+        std::stringstream ss;
+        ss << "Hello World: " << count;
+        msg.data = ss.str();
+        ROS_INFO("%s", msg.data.c_str());
+        pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+    }
+
+    return 0;
+}
