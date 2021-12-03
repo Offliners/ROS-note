7ada2a37d3e18cd19a3e546293c0e2fd224e4565
Offliner <40673034h@ntnu.edu.tw>
2021-12-04 02:01:15 +0800

    Create build_ros2.sh

---
+++ build_ros2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Install ROS-2
+
+# Add the ROS-2 apt repository
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+# Add build dependencies
+sudo apt update && sudo apt install -y build-essential cmake git python3-colcon-common-extensions python3-lark-parser python3-pip python-rosdep python3-vcstool wget
+python3 -m pip install -U argcomplete flake8 flake8-blind-except flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes pytest-repeat pytest-rerunfailures pytest pytest-cov pytest-runner setuptools
+
+# Install ROS-2
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
+vcs import src < ros2.repos
+sudo rosdep init
+rosdep update
+sudo rosdep install --from-paths src --ignore-src --rosdistro -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
