343576941349f1efadaa46d3c0c606c7a036e82e
Offliner <40673034h@ntnu.edu.tw>
2021-12-06 18:26:39 +0800

    Update build_ros1.sh

--- build_ros1.sh
+++ build_ros1.sh
@@ -5,6 +5,8 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -Set up your keys
 
 # Installation
+sudo apt-get install python-pip
+sudo apt-get install rosdep
 sudo apt update
 sudo apt install ros-melodic-desktop-full
 
